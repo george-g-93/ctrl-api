4ecea7ad3d937ecedb50d50b169e5cad46a9d481
George Beechwood <george-g-93@hotmail.com>
2025-10-24 16:37:27 +0100

    Server.js repair

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 // Make sure preflights succeed:
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 
 // Body parsers etc.
